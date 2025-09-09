cd3753e512419c661b425b726e4c6a192453b6c7
HanGil <jhg9240@gmail.com>
2025-09-09 14:48:29 +0900

    Add solution for Level0 Task02

---
+++ src/ko/codegym/task/task01/task0142/Solution.java
@@ -0,0 +1,12 @@
+package ko.codegym.task.task01.task0142;
+
+/* 
+아미고는 아주 영리하다
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //여기에 코드를 작성합니다
+        System.out.println("아미고는 아주 영리하다");
+    }
+}
