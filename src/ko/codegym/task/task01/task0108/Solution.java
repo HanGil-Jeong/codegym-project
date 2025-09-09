1ffde0e9fef0640bcadb21526ba537e428d751a4
HanGil <jhg9240@gmail.com>
2025-09-09 14:47:20 +0900

    Add solution for Level0 Task08

---
+++ src/ko/codegym/task/task01/task0108/Solution.java
@@ -0,0 +1,24 @@
+package ko.codegym.task.task01.task0108;
+
+/* 
+2 + 3 = 5
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 2;
+
+        // System.out.print("둘");
+        System.out.print(b);
+        System.out.print(" 더하기 ");
+        // System.out.print(" 빼기 ");
+        System.out.print(a);
+        // System.out.print("셋");
+        // System.out.print("은 ");
+        System.out.print("은 ");
+        // System.out.print("다섯입니다");
+        System.out.print((a + b) + "입니다");
+    }
+}
