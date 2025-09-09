698555ff30d6e4a6965e9214b5c923ece130cb21
HanGil <jhg9240@gmail.com>
2025-09-09 14:56:12 +0900

    Add solution for Level1 Task10

---
+++ src/ko/codegym/task/task01/task0110/Solution.java
@@ -0,0 +1,13 @@
+package ko.codegym.task.task01.task0110;
+
+/* 
+화면상의 변수
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //여기에 코드를 작성합니다
+        String name = "한길";
+        System.out.println(name);
+    }
+}
