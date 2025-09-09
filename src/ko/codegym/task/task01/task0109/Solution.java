8cc4f4ddd1c21fc86c8f1f1673390f8c07b26ce5
HanGil <jhg9240@gmail.com>
2025-09-09 14:56:05 +0900

    Add solution for Level1 Task09

---
+++ src/ko/codegym/task/task01/task0109/Solution.java
@@ -0,0 +1,11 @@
+package ko.codegym.task.task01.task0109;
+
+/* 
+우리는 이 변수들을 명확하게 공개하고 선언합니다.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //여기에 코드를 작성합니다
+    }
+}
