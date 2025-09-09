8c6a7e14f4269252fc299bb2dace85544f25e4b1
HanGil <jhg9240@gmail.com>
2025-09-09 15:47:08 +0900

    Add solution for Level1 Task14

---
+++ src/ko/codegym/task/task01/task0114/Solution.java
@@ -0,0 +1,21 @@
+package ko.codegym.task.task01.task0114;
+
+/* 
+CodeGym. 한 번 배우면 어디에서나 사용
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //여기에 코드를 작성합니다
+        System.out.println("CodeGym. 한 번 배우면 어디에서나 사용");
+        System.out.println("CodeGym. 한 번 배우면 어디에서나 사용");
+        System.out.println("CodeGym. 한 번 배우면 어디에서나 사용");
+        System.out.println("CodeGym. 한 번 배우면 어디에서나 사용");
+        System.out.println("CodeGym. 한 번 배우면 어디에서나 사용");
+        System.out.println("CodeGym. 한 번 배우면 어디에서나 사용");
+        System.out.println("CodeGym. 한 번 배우면 어디에서나 사용");
+        System.out.println("CodeGym. 한 번 배우면 어디에서나 사용");
+        System.out.println("CodeGym. 한 번 배우면 어디에서나 사용");
+        System.out.println("CodeGym. 한 번 배우면 어디에서나 사용");
+    }
+}
