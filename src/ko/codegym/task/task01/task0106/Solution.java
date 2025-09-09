1b2c327ec74a30bb2a394afecb0724a2944fee8a
HanGil <jhg9240@gmail.com>
2025-09-09 14:46:35 +0900

    Add solution for Level0 Task06

---
+++ src/ko/codegym/task/task01/task0106/Solution.java
@@ -0,0 +1,12 @@
+package ko.codegym.task.task01.task0106;
+
+/* 
+버그와 기능
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //여기에 코드를 작성합니다
+        System.out.print("버그가 아니라 기능");
+    }
+}
