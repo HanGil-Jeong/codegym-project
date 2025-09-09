ad70f674833429af0a86d66e891e1f3a57123593
HanGil <jhg9240@gmail.com>
2025-09-09 15:47:13 +0900

    Add solution for Level1 Task15

---
+++ src/ko/codegym/task/task01/task0115/Solution.java
@@ -0,0 +1,20 @@
+package ko.codegym.task.task01.task0115;
+
+/* 
+어... 새해 복 많이 받으세요!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+//        String s = "즐거운";
+//        System.out.println("크리스마스 보내세요");
+//        System.out.println("보람찬 새해");
+//        System.out.print("주말 보내세요");
+        System.out.print("새해 복 많이");
+//        System.out.print("주말 보내세요");
+//        System.out.println(s);
+//        System.out.print("보람찬 새해되세요!");
+        System.out.print(" ");
+        System.out.println("받으세요");
+    }
+}
