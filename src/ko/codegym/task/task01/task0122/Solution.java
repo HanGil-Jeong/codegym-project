6e00877aa9d72d03d229831f45a9d1330a350e5f
HanGil <jhg9240@gmail.com>
2025-09-09 15:49:43 +0900

    Add solution for Level1 Task22

---
+++ src/ko/codegym/task/task01/task0122/Solution.java
@@ -0,0 +1,14 @@
+package ko.codegym.task.task01.task0122;
+
+/* 
+버그 찾기
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 56;
+        int b = 30;
+        int c = 14;
+        System.out.print(a + b + "c");
+    }
+}
