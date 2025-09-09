8e3b55c9120fe63327749e891c02fdafe1945939
HanGil <jhg9240@gmail.com>
2025-09-09 14:36:03 +0900

    Add solution for Level0 Task03

---
+++ src/ko/codegym/task/task01/task0103/Solution.java
@@ -0,0 +1,16 @@
+package ko.codegym.task.task01.task0103;
+
+/* 
+내 어린 친구
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //여기에 코드를 작성합니다
+        int nowYear = 3126;
+        int a = 8;
+        int b = nowYear - a;
+        System.out.println(b);
+
+    }
+}
