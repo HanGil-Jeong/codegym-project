380b110ed52f6a64fb685c875d646f0868220bef
HanGil <jhg9240@gmail.com>
2025-09-09 14:56:22 +0900

    Add solution for Level1 Task11

---
+++ src/ko/codegym/task/task01/task0111/Solution.java
@@ -0,0 +1,14 @@
+package ko.codegym.task.task01.task0111;
+
+/* 
+변수 하나로는 충분하지 않습니다
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //여기에 코드를 작성합니다
+        int a = 5;
+        int b = 13;
+        String s = "한길";
+    }
+}
