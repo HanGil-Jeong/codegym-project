acda9ee8fbf79dbc0db923036849aa9ed41b71da
HanGil <jhg9240@gmail.com>
2025-09-09 14:39:00 +0900

    Add solution for Level0 Task04

---
+++ src/ko/codegym/task/task01/task0105/Solution.java
@@ -0,0 +1,14 @@
+package ko.codegym.task.task01.task0105;
+
+/* 
+변수 선언
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //여기에 코드를 작성합니다
+        String name = "한길";
+        int age = 28;
+        String city = "여주시";
+    }
+}
