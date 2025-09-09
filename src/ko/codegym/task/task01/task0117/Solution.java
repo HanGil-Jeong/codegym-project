5e769127cad1e839034ad70090f92abe72da1b3f
HanGil <jhg9240@gmail.com>
2025-09-09 15:47:33 +0900

    Add solution for Level1 Task17

---
+++ src/ko/codegym/task/task01/task0117/Solution.java
@@ -0,0 +1,14 @@
+package ko.codegym.task.task01.task0117;
+
+/* 
+몇 군데 더 수정
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String s1 = "자바";
+        String s2 = "로";
+        String s3= "코드 작성";
+        System.out.println(s1 + s2 + " " + s3);
+    }
+}
