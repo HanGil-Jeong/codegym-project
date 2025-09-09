8b56672c7693dd467d420442efd735f082ab5cf6
HanGil <jhg9240@gmail.com>
2025-09-09 15:24:27 +0900

    Add solution for Level1 Task40

---
+++ src/ko/codegym/task/task01/task0140/Solution.java
@@ -0,0 +1,14 @@
+package ko.codegym.task.task01.task0140;
+
+/* 
+숫자의 제곱 출력
+*/
+
+public class Solution {
+    public static int number = 25;
+
+    public static void main(String[] args) {
+        //여기에 코드를 작성합니다
+        System.out.println(number*number);
+    }
+}
