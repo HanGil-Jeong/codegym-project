da59ed2eafeb5898c94d8ec7e85d757ce9cb761f
HanGil <jhg9240@gmail.com>
2025-09-09 15:48:06 +0900

    Add solution for Level1 Task39

---
+++ src/ko/codegym/task/task01/task0139/Solution.java
@@ -0,0 +1,13 @@
+package ko.codegym.task.task01.task0139;
+
+/* 
+키스해, 내 금속 빛깔 후부 작동기
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //여기에 코드를 작성합니다
+        String s = "키스해, 내 금속 빛깔 후부 작동기!";
+        System.out.println(s);
+    }
+}
