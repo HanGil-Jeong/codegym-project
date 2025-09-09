3463ff4aab08e29f90071c4c6508c00d64313248
HanGil <jhg9240@gmail.com>
2025-09-09 15:47:55 +0900

    Add solution for Level1 Task21

---
+++ src/ko/codegym/task/task01/task0121/Solution.java
@@ -0,0 +1,20 @@
+package ko.codegym.task.task01.task0121;
+
+/* 
+계약
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //여기에 코드를 작성합니다
+        System.out.println("제 이름은 아미고입니다.");
+        System.out.println();
+        System.out.println("첫해에 월 5천 달러 급여를 받는 데 동의합니다.");
+        System.out.println("저는 2년 차에 월 5,500달러 급여에 동의합니다.");
+        System.out.println("저는 3년 차에 월 7,000달러 급여에 동의합니다.");
+        System.out.println("저는 4년 차에 월 8,000달러 급여에 동의합니다.");
+        System.out.println("저는 5년 차에 월 10,000달러 급여에 동의합니다.");
+        System.out.println();
+        System.out.println("키스해, 내 금속 빛깔 후부 작동기!");
+    }
+}
