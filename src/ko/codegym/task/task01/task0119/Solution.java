f7ec451dc11d033171d134e9188d578836d74839
HanGil <jhg9240@gmail.com>
2025-09-09 15:47:48 +0900

    Add solution for Level1 Task19

---
+++ src/ko/codegym/task/task01/task0119/Solution.java
@@ -0,0 +1,14 @@
+package ko.codegym.task.task01.task0119;
+
+/* 
+다시 15가 됐군요!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String name = "아미고";
+        int age = 15;
+        int money = 500;
+        int weight = 70;
+    }
+}
