f8c7143dc357bff1d5973c3a09e34ce4f4e3a74f
HanGil <jhg9240@gmail.com>
2025-09-09 15:47:21 +0900

    Add solution for Level1 Task16

---
+++ src/ko/codegym/task/task01/task0116/Solution.java
@@ -0,0 +1,13 @@
+package ko.codegym.task.task01.task0116;
+
+/* 
+코드 변경하기
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String name = "아미고";
+        String text = "안녕하세요, " + name + "!";
+        System.out.println(text);
+    }
+}
