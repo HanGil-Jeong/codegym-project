a32da0aaa5f6a0d60de16018c89940de17309a9e
HanGil <jhg9240@gmail.com>
2025-09-11 14:09:12 +0900

    Add solution for Level1 Task27

---
+++ src/ko/codegym/task/task01/task0127/Solution.java
@@ -0,0 +1,18 @@
+package ko.codegym.task.task01.task0127;
+
+/* 
+숫자의 제곱
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //여기에 코드를 작성합니다
+        int b = sqr(5);
+        
+        System.out.println(b);
+    }
+
+    public static int sqr(int a) {
+        return a * a;
+    }
+}
