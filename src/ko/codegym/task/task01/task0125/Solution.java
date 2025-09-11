8b0a520ec8a396260f07b5e6d6e69c73a867251f
HanGil <jhg9240@gmail.com>
2025-09-11 14:09:00 +0900

    Add solution for Level1 Task25

---
+++ src/ko/codegym/task/task01/task0125/Solution.java
@@ -0,0 +1,18 @@
+package ko.codegym.task.task01.task0125;
+
+/* 
+깔끔하게 코드 정리
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        // String s = "15";
+        int a = 5;
+        // int z = 18;
+        int d = 18;
+        int c = a + d;
+        String b = "보다 나은 건 ";
+
+        System.out.println(c + b + a);
+    }
+}
