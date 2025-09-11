f56dee7281c1579709fef5791bf00036118284df
HanGil <jhg9240@gmail.com>
2025-09-11 14:09:30 +0900

    Add solution for Level1 Task38

---
+++ src/ko/codegym/task/task01/task0135/Solution.java
@@ -0,0 +1,18 @@
+package ko.codegym.task.task01.task0135;
+
+/* 
+건강에 좋은 음식을 선택하십시오! 과일을 선택하십시오!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String apple = "사과";
+        String porridge = "오트밀 죽";
+        String sausage = "소시지";
+        String peach = "배";
+
+        //여기에 코드를 작성합니다
+        System.out.println(apple);
+        System.out.println(peach);
+    }
+}
