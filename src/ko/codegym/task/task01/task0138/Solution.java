0f74f1973ec4d38c593b501e85968fd2f28e2108
HanGil <jhg9240@gmail.com>
2025-09-11 14:09:39 +0900

    Add solution for Level1 Task38

---
+++ src/ko/codegym/task/task01/task0138/Solution.java
@@ -0,0 +1,20 @@
+package ko.codegym.task.task01.task0138;
+
+/* 
+좋아하는 시
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("좋아하는 시: 진달래꽃");
+        //여기에 코드를 작성합니다
+        
+        System.out.println("나 보기가 역겨워");
+        System.out.println("가실 때에는");
+        System.out.println("말없이 고이 보내 드리우리다");
+        System.out.println();
+        System.out.println("영변에 약산");
+        System.out.println("진달래꽃");
+        System.out.println("아름 따다 기실 길에 부리우리다");
+    }
+}
