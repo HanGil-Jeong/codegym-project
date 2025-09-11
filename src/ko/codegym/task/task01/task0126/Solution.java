12cd41d22d121868c4b3a89c337607f44bb88fcb
HanGil <jhg9240@gmail.com>
2025-09-11 14:09:06 +0900

    Add solution for Level1 Task26

---
+++ src/ko/codegym/task/task01/task0126/Solution.java
@@ -0,0 +1,29 @@
+package ko.codegym.task.task01.task0126;
+
+/* 
+내키지 않나요? 어쨌든 작업한다.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //여기에 코드를 작성합니다
+        
+        System.out.println("내키면 작업한다. 내키지 않아도 어쨌든 작업한다");
+        System.out.println("내키면 작업한다. 내키지 않아도 어쨌든 작업한다");
+        System.out.println("내키면 작업한다. 내키지 않아도 어쨌든 작업한다");
+        System.out.println("내키면 작업한다. 내키지 않아도 어쨌든 작업한다");
+        System.out.println("내키면 작업한다. 내키지 않아도 어쨌든 작업한다");
+        System.out.println("내키면 작업한다. 내키지 않아도 어쨌든 작업한다");
+        System.out.println("내키면 작업한다. 내키지 않아도 어쨌든 작업한다");
+        System.out.println("내키면 작업한다. 내키지 않아도 어쨌든 작업한다");
+        System.out.println("내키면 작업한다. 내키지 않아도 어쨌든 작업한다");
+        System.out.println("내키면 작업한다. 내키지 않아도 어쨌든 작업한다");
+        System.out.println("내키면 작업한다. 내키지 않아도 어쨌든 작업한다");
+        System.out.println("내키면 작업한다. 내키지 않아도 어쨌든 작업한다");
+        System.out.println("내키면 작업한다. 내키지 않아도 어쨌든 작업한다");
+        System.out.println("내키면 작업한다. 내키지 않아도 어쨌든 작업한다");
+        System.out.println("내키면 작업한다. 내키지 않아도 어쨌든 작업한다");
+        System.out.println("내키면 작업한다. 내키지 않아도 어쨌든 작업한다");
+
+    }
+}
