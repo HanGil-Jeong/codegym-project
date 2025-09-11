51383cca6c1db75e49bf5c070a89eed5f776559e
HanGil <jhg9240@gmail.com>
2025-09-11 14:09:23 +0900

    Add solution for Level1 Task28

---
+++ src/ko/codegym/task/task01/task0128/Solution.java
@@ -0,0 +1,16 @@
+package ko.codegym.task.task01.task0128;
+
+/* 
+2 + 2 만큼 간단합니다
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        sum(2, 2);
+    }
+
+    public static void sum(int a, int b) {
+        int c = a + b;
+        System.out.print(c);
+    }
+}
